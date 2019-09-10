8b25835a470f427c8ed64a10b6ef7c6b7715be9f
tereom <teresa.ortiz.mancera@gmail.com>
2019-09-10 11:18:38 -0500

    agregar código para datos elecciones 2006

---
+++ R/strata_sample_2006.R
@@ -0,0 +1,13 @@
+#' Sample of 2006 presidential elections.
+#'
+#' A dataset containing the strata information for the 2006 presidential
+#' election sample `sample_2006`
+#'
+#' @format strata_sample_2006: A data frame with 300 rows and 3 columns, one
+#' row per stratum:
+#' \describe{
+#'   \item{stratum}{stratum id}
+#'   \item{n}{Number of polling stations in the sample}
+#'   \item{N}{Total number of polling stations in the stratum}
+#' }
+"strata_sample_2006"
